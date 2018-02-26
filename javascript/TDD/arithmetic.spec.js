65011dac4ffc9d3e289a02cf91905caee26144e6
Kelly <lopezzkn@dukes.jmu.edu>
2018-02-26 10:54:55 -0500

    sum refactored complete

--- javascript/TDD/arithmetic.spec.js
+++ javascript/TDD/arithmetic.spec.js
@@ -1,3 +1,10 @@
+/**
+ * a function to add two numbers
+ * @param {Number} a The first number to be added
+ * @param {Number} b The second number to be added
+ * @param {Number} c The sum of a and b
+ */
+
 // import code to be tested
 const sum = require('./sum');
 
@@ -7,7 +14,6 @@ describe("An arithmetic library", () => {
 	it("Can add two numbers", () => {
 
 		expect(sum(3,4)).toBe(7);
-		expect(sum(8,9).toBe(17);
 		expect(sum(-3,4)).toBe(1);
 	});
 });
--- javascript/TDD/sum.js
+++ javascript/TDD/sum.js
@@ -1,6 +1,27 @@
-
-function sum(a, b) {
+/**
+ * a function to add two numbers
+ * @param {Number} a The first number to be added
+ * @param {Number} b The second number to be added
+ * @param {Number} c The sum of a and b
+ */
+let sum = (a,b) => {
 	return a + b;
-}
+};
 
 module.exports = sum; //thing on the right hand side should be the thing above it
+
+//-----------------alternatives----------------------------
+//
+// function sum(a, b) {
+// 	return a + b;
+// }
+
+// let sum = function(a,b) {
+// 	return a + b;
+// };
+
+// let sum = (a,b) => a + b;
+
+// module.exports = (a,b) => a + b;
+//all of these things are the same things ^^^^
+
